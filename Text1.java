35cbcae722709577098625a5511cc82fdedbd621
YuMiao-login <yumiao010507@163.com>
2021-09-18 11:25:35 +0800

    abc

---
+++ Text1.java
@@ -0,0 +1,51 @@
+public class Text1 {
+    public static void main(String[] args) {
+        /*
+        int a = 100_000_000;
+        System.out.println(a);
+
+        double b = 100_00.00_00000;
+        System.out.println(b);
+
+        char c = 'a';
+        System.out.println(c);
+
+        char d = 64;
+        System.out.println(d);
+
+        char e = '\u0061';
+        System.out.println(e);
+
+        boolean f = true;
+        System.out.println(f);
+
+        boolean g = false;
+        System.out.println(g);
+
+        int class1 = 133;
+        System.out.println(class1);
+
+        int circle = 34;
+        System.out.println(circle);
+
+        System.out.println(100 + 3);
+
+        System.out.println(100 - 3);
+
+        System.out.println(10 * 3);
+
+        System.out.println(6 / 3);
+
+        System.out.println(9 % 2);
+
+         */
+        int a = 10;
+        a += 3;
+        a -= 4;
+        a *= 5;
+        a /= 6;
+        a %= 2;
+        System.out.println(a);
+
+    }
+}
